78dcd9ffab332f004222888e3860773d0ad3f429
Aryan Singh <aryan.singh@innoraft.com>
2025-07-17 11:10:49 +0530

    FRES-269: Add proper namespace and comments.

--- web/modules/custom/welcome_block/src/Plugin/Block/WelcomeBlock.php
+++ web/modules/custom/welcome_block/src/Plugin/Block/WelcomeBlock.php
@@ -21,10 +21,22 @@ class WelcomeBlock extends BlockBase implements ContainerFactoryPluginInterface 
   /**
    * The current user.
    *
-   * @var AccountInterface
+   * @var \Drupal\Core\Session\AccountInterface
    */
   protected $current_user;
 
+  /**
+   * Constructs a new WelcomeBlock instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current logged-in user.
+   */
   public function __construct (array $configuration , $plugin_id, $plugin_definition, AccountInterface $current_user) {
     parent::__construct($configuration, 
     $plugin_id , $plugin_definition);
